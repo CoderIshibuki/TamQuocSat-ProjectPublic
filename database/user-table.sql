991a547cf799afc5fd87cda5eb964f38f8608470
phat2007.dhsp <tranvuhoaphat@gmail.com>
2026-07-03 08:23:31 +0700

    feat: remove comment, add more attribute for user

--- database/user-table.sql
+++ database/user-table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    display_name VARCHAR(100) NOT NULL,
+    gold INT DEFAULT 0 NOT NULL,
+    gems INT DEFAULT 0 NOT NULL,
+    gacha_tickets INT DEFAULT 0 NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_users_username ON users(username);
-
-COMMENT ON TABLE users IS 'player table database';
-COMMENT ON COLUMN users.username IS 'primary key, unique';
-COMMENT ON COLUMN users.password IS 'password is encrypted with Argon2id';
