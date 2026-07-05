f80ba0c02dfc30d15c4b8b0c6752f34a47b23177
phat2007.dhsp <tranvuhoaphat@gmail.com>
2026-07-06 03:05:57 +0700

    feat(general.sql): remove comment, add more attribute for general

--- database/generals.sql
+++ database/generals.sql
@@ -10,12 +10,3 @@ CREATE TABLE IF NOT EXISTS generals (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_generals_id ON generals(general_id);
-
-COMMENT ON TABLE generals IS 'Bảng dữ liệu thẻ Võ Tướng (General Cards) trong Tam Quốc Sát';
-COMMENT ON COLUMN generals.general_id IS 'Mã bài tướng (Bắt đầu bằng GNR+mã số, ví dụ GNR001) - Primary key, unique';
-COMMENT ON COLUMN generals.name IS 'Tên võ tướng (Ví dụ: Lưu Bị, Tào Tháo...)';
-COMMENT ON COLUMN generals.kingdom IS 'Quốc gia / Thế lực (Thục, Ngụy, Ngô, Quần Hùng)';
-COMMENT ON COLUMN generals.max_hp IS 'Huyết lượng tối đa (Máu)';
-COMMENT ON COLUMN generals.gender IS 'Giới tính (Nam / Nữ)';
-COMMENT ON COLUMN generals.image_url IS 'Đường dẫn hình ảnh avatar / thẻ tướng';
-COMMENT ON COLUMN generals.skills IS 'Danh sách và mô tả chi tiết các kỹ năng của võ tướng';
