38a29fe720fa56daabfefc5891dc8b53924e0fe2
phat2007.dhsp <tranvuhoaphat@gmail.com>
2026-07-06 03:13:58 +0700

    feat: add attribute, primary key for match-history table

--- database/match-history.sql
+++ database/match-history.sql
@@ -1,1 +1,10 @@
+CREATE TABLE IF NOT EXISTS match_history (
+    match_id VARCHAR(50) PRIMARY KEY,
+    room_code VARCHAR(20) NOT NULL,
+    game_mode VARCHAR(50) NOT NULL,
+    start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_time TIMESTAMP WITH TIME ZONE,
+    winner_team VARCHAR(50)
+);
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_match_history_id ON match_history(match_id);
