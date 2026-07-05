cdf269cde0798b56fd15f432e8a6951ca648ce70
phat2007.dhsp <tranvuhoaphat@gmail.com>
2026-07-06 03:16:14 +0700

    feat: add attribute, primary key, foreign key for user-collection-progress table

--- database/user-collection-progress.sql
+++ database/user-collection-progress.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS user_collection_progress (
+    user_id INT NOT NULL,
+    milestone_id VARCHAR(50) NOT NULL,
+    is_claimed BOOLEAN DEFAULT FALSE NOT NULL,
+    claimed_at TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (user_id, milestone_id),
+    CONSTRAINT fk_user_collection_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_collection_milestone FOREIGN KEY (milestone_id) REFERENCES collection_milestones(milestone_id) ON DELETE CASCADE
+);
